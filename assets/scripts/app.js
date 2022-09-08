15f4e7473f5cedcaa91c1f74de0e1cb4d652e1bf
Faril Mammadov <78743061+farildev@users.noreply.github.com>
2022-09-08 19:02:19 +0400

    v1.0.7

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,35 @@
-const api = 'https://api.openweathermap.org/data/2.5/';
+const url = 'https://api.openweathermap.org/data/2.5/';
 const key = 'df3a643770aa5aca3d0a6d2383650a0b';
+
 const getResult = (cityName) => {
-    console.log(cityName);
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=en`
+    fetch(query)
+    .then(weather => {
+        return weather.json()
+    })
+    .then (displayResult)
 }
 
-const setQuery = (e) => {
-    if(e.keycode == '13')
+const setQuery = function(e) {
+    if(e.key == 'Enter')
     getResult(searchBar.value)
 }
 
+const displayResult = (result) => {
+    let city = document.querySelector('.city')
+    city.innerText = `${result.name}, ${result.sys.country}`
 
-const searchBar = document.getElementById('search-bar');
+    let temp = document.querySelector('.temp')
+    temp.innerText = `${Math.round(result.main.temp)}`
+
+    let humidity = document.querySelector('.humidity')
+    temp.innerText = `${Math.round(result.main.temp)}%`
+
+    let wind = document.querySelector('.wind')
+    wind.innerText = `${result.wind.speed} km/h`
+}
+
+const searchBar = document.getElementById('searchbar');
 searchBar.addEventListener('keypress', setQuery);
 
 
